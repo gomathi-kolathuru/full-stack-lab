6a5497ad79c8644027213dcbd4e3ed4c2deadbc7
gomathi-kolathuru <gomathikolathuru@gmail.com>
2026-07-29 15:47:11 +0530

    Added Week 2A JavaScript Program

---
+++ week2a.js
@@ -0,0 +1,11 @@
+// Week 2A JavaScript Program
+
+console.log("Week 2A JavaScript Program");
+
+let a = 10;
+let b = 20;
+let sum = a + b;
+
+console.log("First Number:", a);
+console.log("Second Number:", b);
+console.log("Sum =", sum);
