52259196e4bce9341b80c0b036a4d583ffde9541
gomathi-kolathuru <gomathikolathuru@gmail.com>
2026-08-18 10:42:22 +0530

    Add demo1.js

---
+++ demo1.js
@@ -0,0 +1,8 @@
+console.log("Week 2A JavaScript Program");
+let a = 10;
+let b = 20;
+let sum = a + b;
+
+console.log("First Number:", a);
+console.log("Second Number:", b);
+console.log("Sum =", sum);
